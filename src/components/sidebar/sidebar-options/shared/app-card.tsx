582ae0da421b9c5db14593c338f0c3076d2ab9af
Evren-os <evrnos@proton.me>
2025-02-06 00:48:43 +0600

    fix(app-card): web favicon not showing

--- src/components/sidebar/sidebar-options/shared/app-card.tsx
+++ src/components/sidebar/sidebar-options/shared/app-card.tsx
@@ -1,18 +1,27 @@
-import { Icon } from "@iconify/react/dist/iconify.js"
-import type React from "react"
-import Button from "~/components/ui/button"
+import { Icon } from "@iconify/react/dist/iconify.js";
+import type React from "react";
+import Button from "~/components/ui/button";
 
 interface AppCardProps {
-  icon: string
-  children: React.ReactNode
-  delFunc: () => void
+  icon: string;
+  children: React.ReactNode;
+  delFunc: () => void;
 }
 
 const AppCard = ({ icon, children, delFunc }: AppCardProps) => {
   return (
     <div className="relative flex flex-col items-center rounded-xl bg-background p-4">
       <span className="flex size-11 items-center justify-center gap-1 rounded-full bg-card text-foreground transition-colors duration-300">
-        <Icon icon={icon || "mynaui:daze-ghost"} fontSize={24} />
+        {icon.startsWith("webicon:") ? (
+          <img
+            src={`https://www.google.com/s2/favicons?domain=${
+              icon.split(":")[1]
+            }&sz=128`}
+            alt="app-icon"
+          />
+        ) : (
+          <Icon icon={icon || "mynaui:daze-ghost"} fontSize={24} />
+        )}
       </span>
       <div className="flex w-full flex-col space-y-2 pt-6">{children}</div>
       <Button
@@ -23,6 +32,6 @@ const AppCard = ({ icon, children, delFunc }: AppCardProps) => {
         onClick={delFunc}
       />
     </div>
-  )
-}
-export default AppCard
+  );
+};
+export default AppCard;
